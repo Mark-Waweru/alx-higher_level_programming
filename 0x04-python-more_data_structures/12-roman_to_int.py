55d859f4fa158a1c399040a4edfe31f77801d65c
Mark-Waweru <markwawerumwai@gmail.com>
2023-12-30 12:48:24 -0800

    Mandatory tasks

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,11 +4,12 @@ def roman_to_int(roman_string):
         return (0)
     total_num = 0
     num = 0
-    d_dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
-            'D': 500, 'M': 1000}
+    d_dictionary = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+            'D': 500, 'M': 1000
+    }
     for i in reversed(roman_string):
         num = d_dictionary[i]
         total_num += num if total_num < num * 5 else -num
 
     return (total_num)
-
