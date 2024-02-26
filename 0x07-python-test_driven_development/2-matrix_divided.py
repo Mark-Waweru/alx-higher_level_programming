9c2100bef4d9a932ba4568cb37fdf4bdd92727da
Mark-Waweru <markwawerumwai@gmail.com>
2024-02-26 15:03:56 -0800

    Solving erros for 3 tasks

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -19,7 +19,10 @@ def matrix_divided(matrix, div):
     '''
     err_msg = "matrix must be a matrix(list of lists) of integers/floats"
 
-    if not isinstance(matrix, list):
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all((isinstance(ele, int) or isinstance(ele, float))
+                    for ele in [num for row in matrix for num in row])):
         raise TypeError(err_msg)
 
     for row in matrix:
